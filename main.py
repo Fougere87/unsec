450ba6c94ad2a25978db4d77380af35b9e50a2c1
Fougere <hugo.mayere@gmail.com>
2015-10-07 10:21:32 +0200

    Avancées sur la classe Email

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import os
 from unsec import Email
 
 
+# for file in os.walk("data/bioinfo_2014-01/*.recoded"):
+#     print(file)
+
+# for i in range()
+#
 e = Email("data/bioinfo_2014-01/9.recoded")
 
-print(e.sender)
+# print(e.sender)
+print(e.subject)
+# print(e.body)
--- unsec/Email.py
+++ unsec/Email.py
@@ -5,25 +5,44 @@ class Email(object):
     def __init__(self, filename):
         file = open(filename, "r")
         raw = "".join(file.readlines())
-
+        # dec = re.match("(.*) (From:\s.+\n) (Subject:\s.+\n) (.*)", raw, re.DOTALL)
         self.subject = Email.extract_subject(raw)
         self.body    = Email.extract_body(raw)
         self.sender  = Email.extract_sender(raw)
+
+    def salut(self):
+        print(self.nom)
+
+
 
 
     @staticmethod
     def extract_subject(raw):
         ''' extract subject '''
-        return "je suis je subject"
+        # line = re.search("Subject:\s.+", raw)
+        # dec = re.search("(.*) (From:\s.+\n) (Subject:\s.+\n) (.*)", raw, re.DOTALL)
+        dec = re.search("(From:\s.+)(\n.*)(Subject:\s.+\n)", raw, re.DOTALL)
+        return dec.group(1)
+
+    # def extract_subject(dec):
+    #     ''' extract subject '''
+    #     # line = re.search("Subject:\s.+", raw)
+    #     return dec.group(3)
+
 
     @staticmethod
     def extract_body(raw):
         ''' extract body '''
-        return "body"
+        subj = re.split("Subject:\s.+", raw)
+        line = subj[1]
+        return line
 
     @staticmethod
     def extract_sender(raw):
         ''' extract email with a regexp '''
-        line = re.findall(r'Sender: \w+@\w+\.\w{2,3}',raw)[0]
-        line = line.replace("Sender: ", "")
-        return line
+        line = re.search("From:\s.+",raw)
+        return line.group(0)
+
+
+    def __str__(self):
+        return "mail de " + self.sender
