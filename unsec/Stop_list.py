dd478476547740e535791fc46df298b15c13d627
enaitso <ostiane.daugustin@gmail.com>
2015-10-08 09:32:48 +0200

    read stop lists

--- unsec/Stop_list.py
+++ unsec/Stop_list.py
@@ -10,14 +10,3 @@ class Stop_list(object):
         with open(filename, 'r') as myfile:
             print(myfile.read().split("\n"))
         return(None)
-
-    # @staticmethod
-    # def create_list(raw):
-    #     stop_list = []
-    #     for line in raw:
-    #         stop_list.append(line)
-    #     print(stop_list)
-    #     return(stop_list)
-    #
-    # def create_array(self):
-    #     return(Stop_list.create_list(self.stop_words))
