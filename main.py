e5994eba085dbf89d1350fc0b9935d907e81be54
Sacha Schutz <sacha@labsquare.org>
2015-10-21 15:14:04 +0200

    move stopList and rename

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from unsec import Email
-from unsec.stop_list import *
+from unsec.StopList import *
 from unsec import Collection
 
 # for file in os.walk("data/bioinfo_2014-01/*.recoded"):
@@ -14,18 +14,20 @@ from unsec import Collection
 # Fonctionne
 ###
 e = Email("data/bioinfo_2014-01/2.recoded")
-fr1 = create_list("stop_list/french1")
-fr2 = create_list("stop_list/french2")
-en1 = create_list("stop_list/english1")
-en2 = create_list("stop_list/english2")
-en3 = create_list("stop_list/english3")
-fr = merge_lists(fr1, fr2)
-en = merge_lists(en1, en2, en3)
+
+
+# fr1 = create_list("unsec/stop_list/french1")
+# fr2 = create_list("unsec/stop_list/french2")
+# en1 = create_list("unsec/stop_list/english1")
+# en2 = create_list("unsec/stop_list/english2")
+# en3 = create_list("unsec/stop_list/english3")
+# fr = merge_lists(fr1, fr2)
+# en = merge_lists(en1, en2, en3)
 
 ###
 # Test en cours
 ###
-col = Collection.Collection("data/")
+#col = Collection.Collection("data/")
 
 # print(Email.remove_stopwords(e.tokenBody(), fr = fr ,en = en))
 #
