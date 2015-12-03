760d837c5df0091bbd06c6506de76756e60aaa86
Fougere <hugo.mayere@gmail.com>
2015-12-03 23:27:50 +0100

    merging

--- hugo_test.py
+++ hugo_test.py
@@ -28,24 +28,23 @@ collection.add_from_files("data/complete/bioinfo_2014-0*")
 
 
 engine   = Clusterizer(collection)
-engine.target = "body"
+engine.target = "both"
 engine.set_vectorizer(TfidfVectorizer())
 engine.set_algorithm(HierarchicalAlgo(n_clusters = 2, affinity ="cosine"))
 engine.run_cleaner()
 engine.run_vectorizer()
-prev_silhouette_res = -0.2
-sd_treshold = 0.15
+prev_silhouette_res = -0.15
+sd_treshold = 0.10
 
-n_clust = 20 #numbre of clustering to iterate
-clust_to_reclust = [[] for i in range(2,n_clust)]
+n_clust = 50 #numbre of clustering to iterate
+clust_to_reclust = []
 
-for n_clusters in range(18,n_clust) :
+for n_clusters in range(2,n_clust) :
 
 #==============================Computing new n clusters
 
     engine.set_algorithm(HierarchicalAlgo(n_clusters = n_clusters, affinity ="cosine"))
     engine.run_algorithm()
-    engine.compute_clusters()
     matrix = np.array(engine.vectorizer.matrix)
     labels = engine.labels
 
@@ -74,35 +73,35 @@ for n_clusters in range(18,n_clust) :
         for clust in range(n_clusters) :
             ith_cluster_silhouette_values = sample_silhouette_values[labels == clust]
             ith_cluster_silhouette_mean = np.mean(ith_cluster_silhouette_values)
-            if ith_cluster_silhouette_mean < 0 :
-                engine.compute_clusters()
-                clust_to_reclust[n_clusters].append(engine.clusters[clust])
+            if ith_cluster_silhouette_mean < 0 and len([labels==clust]) > 30 :
+                clust_to_reclust.append(engine.clusters[clust])
+                print("cluster,", clust, "is to be reclustered as it's silhouette score is ", ith_cluster_silhouette_mean)
 
-        # ax1 = plt
-        # y_lower = 10
-        # for i in range(n_clusters):
-        #     # Aggregate the silhouette scores for samples belonging to
-        #     # cluster i, and sort them
-        #     ith_cluster_silhouette_values = sample_silhouette_values[labels == i]
-        #     ith_cluster_silhouette_values.sort()
-        #     size_cluster_i = ith_cluster_silhouette_values.shape[0]
-        #     y_upper = y_lower + size_cluster_i
-        #     color = cm.spectral(float(i) / n_clusters)
-        #     ax1.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values,
-        #                           facecolor=color, edgecolor=color, alpha=0.7)
-        #
-        #     # Label the silhouette plots with their cluster numbers at the middle
-        #     ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-        #
-        #     # Compute the new y_lower for next plot
-        #     y_lower = y_upper + 10  # 10 for the 0 samples
-        #
-        # ax1.title("The silhouette plot for the various clusters.")
-        # ax1.xlabel("The silhouette coefficient values")
-        # ax1.ylabel("Cluster label")
-        # ax1.axvline(x=silhouette_res, color="red", linestyle="--")
-        #
-        # plt.show()
+        ax1 = plt
+        y_lower = 10
+        for i in range(n_clusters):
+            # Aggregate the silhouette scores for samples belonging to
+            # cluster i, and sort them
+            ith_cluster_silhouette_values = sample_silhouette_values[labels == i]
+            ith_cluster_silhouette_values.sort()
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+            color = cm.spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values,
+                                  facecolor=color, edgecolor=color, alpha=0.7)
+
+            # Label the silhouette plots with their cluster numbers at the middle
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+            # Compute the new y_lower for next plot
+            y_lower = y_upper + 10  # 10 for the 0 samples
+
+        ax1.title("The silhouette plot for the various clusters.")
+        ax1.xlabel("The silhouette coefficient values")
+        ax1.ylabel("Cluster label")
+        ax1.axvline(x=silhouette_res, color="red", linestyle="--")
+
+        plt.show()
     prev_silhouette_res = silhouette_res
 
 def unclusterded_clusters_detection(clusterizer) :
@@ -123,7 +122,7 @@ def reclusterise(clusters, target = "body", vectorizer = TfidfVectorizer(), algo
     new_clusts = []
     for nc in clusters :
         print(nc)
-        reclusterizer = Clusterizer(c, target = "body")
+        reclusterizer = Clusterizer(nc, target = "both")
         reclusterizer.set_algorithm(algorithm(n_clusters = 2, affinity = affinity))
         reclusterizer.set_vectorizer(vectorizer)
         reclusterizer.compute()
@@ -134,9 +133,7 @@ def reclusterise(clusters, target = "body", vectorizer = TfidfVectorizer(), algo
 
 sub_clusters =[]
 for nc in clust_to_reclust :
-    if len(nc) :
-        for c in nc :
-             sub_clusters.append(reclusterise(c, n_clusters = 15))
+    sub_clusters.append(reclusterise(nc, n_clusters = 15))
 for clust_ens in sub_clusters :
     for clust in clust_ens :
         print("============================================")
