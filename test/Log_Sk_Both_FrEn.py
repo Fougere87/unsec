8011216a0a3da7d763e0c049be772db6e5d2ce4d
fougere87 <hugo.mayere@gmail.com>
2015-12-03 23:01:07 +0100

    added tests config

---
+++ test/Log_Sk_Both_FrEn.py
@@ -0,0 +1,24 @@
+import unsec
+from unsec.vectorizer import *
+from unsec.algorithm import *
+
+# print console output
+DEBUG      = True
+# place of emails to clusterize
+PATH       = "data/complete"
+# number of clusters
+N_CLUSTERS = 4
+# apply clustering on body, subject or both
+TARGET     = "both"
+# filter by language
+# LANG       = "fr"
+# Which Vectorizer to use : LogicalVectorizer or TfidfVectorizer
+VECTORIZER = LogicVectorizer()
+# which algorithm to use : SKMeanAlgo or HierarchicalAlgo
+ALGORITHM  = SKMeanAlgo()
+# Results output file
+JSON_FILE  = "clustering.json"
+
+ENABLE_TEST = True
+TEST_CLUSTERING_RANGE = range(2,25)
+TEST_FOLDER = "Log_Sk_Both_FrEn"
