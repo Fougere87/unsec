2de3c96b763c3e519b3d0eb4a82df5483e570328
Sacha Schutz <sacha@labsquare.org>
2015-12-04 10:58:14 +0100

    update plot_test.r

--- plot_test.r
+++ plot_test.r
@@ -1,29 +1,32 @@
 library("jsonlite")
-#
-# data = read.table("test/Log_Hierar_Both_FrEn/clustering.test", header=T, sep="\t")
-#
-# par(mfrow=c(1,3))
-#
-# plot(data$intra, main="mean Intra distance", type="l", xlab ="number of cluster")
-# plot(data$extra, main="exta distance", type="l", xlab ="number of cluster")
-# plot(data$silhouette, main="Silhouette score", type="l", xlab ="number of cluster")
 
+TEST_FOLDER = "test/Log_Hierar_Both_FrEn/"
 
-SIZE = 30
+##Draw graph 
+data = read.table(paste(TEST_FOLDER,"clustering.test",sep=""), header=T, sep="\t")
 
-file.names <- dir("test/TFCl_Hierar_Body_FrEn/", pattern =".json")
+par(mfrow=c(1,3))
+
+plot(data$intra, main="mean Intra distance", type="l", xlab ="number of cluster")
+plot(data$extra, main="exta distance", type="l", xlab ="number of cluster")
+plot(data$silhouette, main="Silhouette score", type="l", xlab ="number of cluster")
+
+n_cluster = data$n_cluster
+
+file.names <- dir(TEST_FOLDER, pattern =".json$")
 for(i in 1:length(file.names)){
-  filename = paste("test/TFCl_Hierar_Body_FrEn/",file.names[i], sep = "")
-  json = fromJSON(filename)
+filename = paste(TEST_FOLDER,file.names[i], sep = "")
+json = fromJSON(filename)
 
-  data = json$clusters$count
-  combi = sort(c(data, rep(0,30 - length(data))), decreasing = T)
+data = json$clusters$count
+#combi = sort(c(data, rep(0,length(n_cluster) - length(data))), decreasing = T)
 
-  print(data)
-  png(filename=paste(filename,".png", sep=""))
-  
-  barplot(combi, main=paste("n = ", 2+i), ylim = c(0,500))
+append = rep(0,length(n_cluster) +1 - length(data))
+combi   = sort(c(data, append), decreasing = T)
 
-  dev.off()
+print(combi)
 
+png(filename=paste(filename,".png", sep=""))
+barplot(combi, main=paste("n = ", n_cluster[i]), ylim = c(0,500))
+dev.off()
 }
