40c558f77eb83a210acf60f9a1b8033d1bab7385
Sacha Schutz <sacha@labsquare.org>
2015-12-04 10:06:28 +0100

    add results files

--- plot_test.r
+++ plot_test.r
@@ -1,11 +1,28 @@
+library("jsonlite")
+# 
+# data = read.table("test/Log_Hierar_Both_FrEn/clustering.test", header=T, sep="\t")
+# 
+# par(mfrow=c(1,3))
+# 
+# plot(data$intra, main="mean Intra distance", type="l", xlab ="number of cluster")
+# plot(data$extra, main="exta distance", type="l", xlab ="number of cluster")
+# plot(data$silhouette, main="Silhouette score", type="l", xlab ="number of cluster")
 
-FOLDER = "test_results"
+SIZE = 30
 
-data = read.table("test_results2/clustering.test", header=T, sep="\t")
+file.names <- dir("test/TFCl_Hierar_Body_FrEn/", pattern =".json")
+for(i in 1:length(file.names)){
+  filename = paste("test/TFCl_Hierar_Body_FrEn/",file.names[i], sep = "")
+  json = fromJSON(filename)
+  
+  data = json$clusters$count
+  combi = sort(c(data, rep(0,30 - length(data))), decreasing = T)
+  
+  print(data)
+  png(filename=paste(filename,".png", sep=""))
 
-par(mfrow=c(1,3))
+  barplot(combi, main=paste("n = ", 2+i), ylim = c(0,500))
+  
+  dev.off()
 
-plot(data$intra, main="mean Intra distance", type="l", xlab ="number of cluster")
-plot(data$extra, main="exta distance", type="l", xlab ="number of cluster")
-plot(data$silhouette, main="Silhouette score", type="l", xlab ="number of cluster")
-
+}
